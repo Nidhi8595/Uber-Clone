6119a58cfa4a73bfa0e479ae4a0f521fc4507d7d
Nidhi8595 <155657850+Nidhi8595@users.noreply.github.com>
2025-03-23 19:38:35 +0530

    user authentication

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
  const cors = require('cors');
  const app = express();
  
+ const cookieParser = require('cookie-parser');
  const connectToDb = require('./db/db');
  const userRoutes = require('./routes/user.routes');
 
@@ -12,7 +13,7 @@ const dotenv = require('dotenv');
  app.use(cors());
  app.use(express.json());
  app.use(express.urlencoded({ extended: true }));
- 
+ app.use(cookieParser());
  
  
  app.get('/', (req, res) => {
